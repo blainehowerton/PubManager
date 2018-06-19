90acd650be3dfe761230b7a470a61bd233824ca7
Blaine Howerton <blainehowerton@Blaines-iMac.local>
2018-06-19 00:46:16 -0600

    removed chat functionality from original app

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :customers
   resources :equipment
-  resources :chat
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
  # You can have the root of your site routed with "root"
