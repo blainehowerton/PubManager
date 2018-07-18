5cef1378def0b16e6ca8b221d8d2e95d2ec410ce
Blaine Howerton <blainehowerton@Blaines-iMac.local>
2018-07-18 01:46:01 -0600

    fixed error in model and view for subscriptions

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,3 @@
 class Customer < ApplicationRecord
-  has_many :subscriptions, :class_name => "Customer",
-    :foreign_key => "customer_id"
-  has_many :publications, :through => :subscriptions
+  
 end
