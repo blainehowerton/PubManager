897005e36ddf6d7c7b79832234bb0e8e19e9db35
Blaine Howerton <blainehowerton@Blaines-iMac.local>
2018-07-21 17:06:13 -0600

    added edition search capability to orders view.

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ApplicationRecord
   belongs_to :customer
   belongs_to :publication
   belongs_to :edition
+
+  def self.search(search)
+    if search
+        where(["edition_id LIKE ?","%#{search}%"])
+      else
+        all
+      end
+    end
 end
